55a0a0ee2b82d9b66403d11c8dc166a2b7fc89d2
AlexMolio <mr.molokhov@mail.ru>
2022-12-07 23:55:19 +0300

    make type selection

--- main.m
+++ main.m
@@ -6,7 +6,26 @@ N = 100; % numder of objects
 
 for i = 1:N
     % todo: select random type of object
+    type = randi(3);
+    
     % todo: set random number of points, width and length
+    switch type
+        case 1
+            ON = randi(5) + 5;
+            OW = randi(5) + 5;
+            OH = randi(50) + 50;
+
+        case 2 
+            ON = randi(4) + 1;
+            OW = randi(29) + 1;
+            OH = randi(2) + 1;
+            
+        case 3
+            ON = randi(20) + 30;
+            OW = randi(20) + 90;
+            OH = randi(80) + 70;
+    end
+
     % todo: set random position and rotation angle
     % todo: generate echo signal with params: fd = 120 000, fs = 30 000, T = 4
     % todo: process input signal
